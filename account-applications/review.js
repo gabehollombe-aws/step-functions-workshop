8fac793266f1175ebf9da577b5e732c7ebd181e0
Gabe Hollombe <gabe@avantbard.com>
2019-10-16 10:58:50 +0800

    Call out to Lambda from Pending Review state, add Review Approved? choice state that transitions to Approve or Reject pass states. Create a review lambda that calls back to Step Functions with review decision in SendTaskSuccess

--- account-applications/review.js
+++ account-applications/review.js
@@ -6,14 +6,15 @@ const AWS = require('aws-sdk')
 AWS.config.update({region: REGION});
 
 const dynamo = new AWS.DynamoDB.DocumentClient();
+const stepfunctions = new AWS.StepFunctions();
 
 const getApplication = async (id) => {
     const params = {
       TableName: ACCOUNTS_TABLE_NAME,
       Key: { id: id }
     };
-
-    const result = await dynamo.get(params).promise()
+    
+    const result = await dynamo.get(params).promise()    
     return result.Item
 }
 
@@ -34,39 +35,33 @@ const updateApplication = async (id, attributes) => {
     return updatedApplication
 }
 
-const flagForReview = async (data) => {
-    const { id, flagType, taskToken } = data
-
-    if (flagType !== 'REVIEW' && flagType !== 'UNPROCESSABLE_DATA') {
-        throw new Error("flagType must be REVIEW or UNPROCESSABLE_DATA")
+const updateApplicationWithDecision = (id, decision) => {
+    if (decision !== 'APPROVE' && decision !== 'REJECT') {
+        throw new Error("Required `decision` parameter must be 'APPROVE' or 'REJECT'")
     }
 
-    let newState
-    let reason
-    if (flagType === 'REVIEW') {
-        newState = 'FLAGGED_FOR_REVIEW'
-        reason = data.reason
+    switch(decision) {
+        case 'APPROVE': return updateApplication(id, { state: 'REVIEW_APPROVED' })
+        case 'REJECT': return updateApplication(id, { state: 'REVIEW_REJECTED' })
     }
-    else {
-        reason = JSON.parse(data.errorInfo.Cause).errorMessage
-        newState = 'FLAGGED_WITH_UNPROCESSABLE_DATA'
-    }
+}
 
-    const updatedApplication = await updateApplication(
-        id,
-        {
-            state: newState,
-            reason,
-            taskToken
-        }
-    )
+const updateWorkflowWithReviewDecision = async (data) => {
+    const { id, decision } = data
 
+    const updatedApplication = await updateApplicationWithDecision(id, decision)
+
+    let params = {
+        output: JSON.stringify({ decision }),
+        taskToken: updatedApplication.taskToken
+    };
+    await stepfunctions.sendTaskSuccess(params).promise()
     return updatedApplication
 }
 
 module.exports.handler = async(event) => {
     try {
-        const result = await flagForReview(event)
+        const result = await updateWorkflowWithReviewDecision(event)
         return result
     } catch (ex) {
         console.error(ex)
