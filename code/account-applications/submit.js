17bb1788bbb8ff1e8c815490f2d44057f5c92afb
Gabe Hollombe <gabe@avantbard.com>
2019-10-31 14:40:07 +0800

    new account-applications/submit.js v1

--- code/account-applications/submit.js
+++ code/account-applications/submit.js
@@ -1,13 +1,11 @@
 'use strict';
 const REGION = process.env.REGION
 const ACCOUNTS_TABLE_NAME = process.env.ACCOUNTS_TABLE_NAME
-const APPLICATION_PROCESSING_STEP_FUNCTION_ARN = process.env.APPLICATION_PROCESSING_STEP_FUNCTION_ARN
 
 const AWS = require('aws-sdk')
 AWS.config.update({region: REGION});
 
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const stepfunctions = new AWS.StepFunctions();
 
 const AccountApplications = require('./AccountApplications')(ACCOUNTS_TABLE_NAME, dynamo)
 
@@ -17,28 +15,18 @@ const submitNewAccountApplication = async (data) => {
     return application
 } 
 
-const startStateMachineExecution = (application) => {
-    const params = {
-        "input": JSON.stringify({ application }),
-        "name": `ApplicationID-${application.id}`,
-        "stateMachineArn": APPLICATION_PROCESSING_STEP_FUNCTION_ARN
-    }
-    return stepfunctions.startExecution(params).promise()
-}
-
 module.exports.handler = async(event) => {
     let application
     try {
         application = await submitNewAccountApplication(event)
-        await startStateMachineExecution(application)
         return application
     } catch (ex) {
-        console.error(ex)
-        console.info('event', JSON.stringify(event))
-
         if (application !== undefined) {
             await AccountApplications.delete(application.id)
         }
+
+        console.error(ex)
+        console.info('event', JSON.stringify(event))
         throw ex
     }
 }
