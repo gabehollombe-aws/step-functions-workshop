509c5f4da832d190d3285f30d91fd29c3253b6fb
Gabe Hollombe <gabe@avantbard.com>
2019-10-31 14:41:42 +0800

    new account-applications/submit.js v2

--- code/account-applications/submit.js
+++ code/account-applications/submit.js
@@ -1,11 +1,13 @@
 'use strict';
 const REGION = process.env.REGION
 const ACCOUNTS_TABLE_NAME = process.env.ACCOUNTS_TABLE_NAME
+const APPLICATION_PROCESSING_STEP_FUNCTION_ARN = process.env.APPLICATION_PROCESSING_STEP_FUNCTION_ARN
 
 const AWS = require('aws-sdk')
 AWS.config.update({region: REGION});
 
 const dynamo = new AWS.DynamoDB.DocumentClient();
+const stepfunctions = new AWS.StepFunctions();
 
 const AccountApplications = require('./AccountApplications')(ACCOUNTS_TABLE_NAME, dynamo)
 
@@ -15,10 +17,20 @@ const submitNewAccountApplication = async (data) => {
     return application
 } 
 
+const startStateMachineExecution = (application) => {
+    const params = {
+        "input": JSON.stringify({ application }),
+        "name": `ApplicationID-${application.id}`,
+        "stateMachineArn": APPLICATION_PROCESSING_STEP_FUNCTION_ARN
+    }
+    return stepfunctions.startExecution(params).promise()
+}
+
 module.exports.handler = async(event) => {
     let application
     try {
         application = await submitNewAccountApplication(event)
+        await startStateMachineExecution(application)
         return application
     } catch (ex) {
         if (application !== undefined) {
