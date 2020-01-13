94e8dc5ffab544ce4863cb730d8e54e721055cc3
Gabe Hollombe <gabe@avantbard.com>
2020-01-13 11:10:29 +0800

    adding files for custom serverless template

---
+++ serverless_template/account-applications/submit.js
@@ -0,0 +1,32 @@
+'use strict';
+const REGION = process.env.REGION
+const ACCOUNTS_TABLE_NAME = process.env.ACCOUNTS_TABLE_NAME
+
+const AWS = require('aws-sdk')
+AWS.config.update({region: REGION});
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const AccountApplications = require('./AccountApplications')(ACCOUNTS_TABLE_NAME, dynamo)
+
+const submitNewAccountApplication = async (data) => {
+    const { name, address } = data
+    const application = await AccountApplications.create({ name, address, state: 'SUBMITTED' })
+    return application
+} 
+
+module.exports.handler = async(event) => {
+    let application
+    try {
+        application = await submitNewAccountApplication(event)
+        return application
+    } catch (ex) {
+        if (application !== undefined) {
+            await AccountApplications.delete(application.id)
+        }
+
+        console.error(ex)
+        console.info('event', JSON.stringify(event))
+        throw ex
+    }
+}
